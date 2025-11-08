e9238bf2b826a4197d39be5f2be9aead28451739
ArchCharNote <59707178+ArchCharNote@users.noreply.github.com>
2025-11-08 23:33:57 +0400

    component Main

---
+++ src/layout/sections/main/Main.tsx
@@ -0,0 +1,40 @@
+import { styled } from "styled-components";
+import photo from '../../../assets/images/photo.webp'
+import { FlexWrapper } from "../../../components/FlexWrapper";
+
+export const Main = () => {
+    return (
+        <StyledMain>
+            <FlexWrapper align={'center'} justify={'space-around'}>
+                <div>
+                    <span>Hi there</span>
+                    <Name>I am Artem Tarasenko</Name>
+                    <MainTitle>A Web Developer</MainTitle>
+                </div>
+
+                <Photo src={photo} alt="photo" />
+            </FlexWrapper>
+        </StyledMain>
+    );
+};
+
+
+
+const StyledMain = styled.div`
+    min-height: 100vh;
+    background-color: #ffffe7;
+`
+
+const Photo = styled.img`
+    width: 350px;
+    height: 430px;
+    object-fit: cover;
+`
+
+const MainTitle = styled.h1`
+    
+`
+
+const Name = styled.h2`
+    
+`
