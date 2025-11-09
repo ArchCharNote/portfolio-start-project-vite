e6dfd283273a9fcc23f7075cf10f726dec70f92f
ArchCharNote <59707178+ArchCharNote@users.noreply.github.com>
2025-11-09 06:01:47 +0400

    skills component

---
+++ src/components/SectionTitle.tsx
@@ -0,0 +1,6 @@
+import { styled } from 'styled-components';
+
+export const SectionTitle = styled.h2`
+  color: #ad6fe7;
+  text-align: center;
+`;
---
+++ src/layout/sections/skills/Skills.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import styled from 'styled-components';
+import { FlexWrapper } from '../../../components/FlexWrapper';
+import { SectionTitle } from '../../../components/SectionTitle';
+import { Skill } from './skill/Skill';
+
+export const Skills = () => {
+  return (
+    <StyledSkills>
+      <SectionTitle>My Skills</SectionTitle>
+      <FlexWrapper wrap="wrap" justify="space-between" gap="20px">
+        <Skill
+          iconId="html"
+          title="html5"
+          description="Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum  Lorem ipsumLorem ipsumLorem ipsumLorem ipsum Lorem ipsumLorem ipsumLorem ipsum Lorem ipsumLorem ipsumмLorem ipsumLorem ipsum Lorem ipsum"
+        />
+
+        <Skill
+          iconId="css"
+          title="css3"
+          description="Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum  Lorem ipsumLorem ipsumLorem ipsumLorem ipsum Lorem ipsumLorem ipsumLorem ipsum Lorem ipsumLorem ipsumмLorem ipsumLorem ipsum Lorem ipsum"
+        />
+
+        <Skill
+          iconId="react"
+          title="react"
+          description="Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum  Lorem ipsumLorem ipsumLorem ipsumLorem ipsum Lorem ipsumLorem ipsumLorem ipsum Lorem ipsumLorem ipsumмLorem ipsumLorem ipsum Lorem ipsum"
+        />
+
+        <Skill
+          iconId="styled"
+          title="styled components"
+          description="Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum  Lorem ipsumLorem ipsumLorem ipsumLorem ipsum Lorem ipsumLorem ipsumLorem ipsum Lorem ipsumLorem ipsumмLorem ipsumLorem ipsum Lorem ipsum"
+        />
+
+        <Skill
+          iconId="ts"
+          title="typescript"
+          description="Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum  Lorem ipsumLorem ipsumLorem ipsumLorem ipsum Lorem ipsumLorem ipsumLorem ipsum Lorem ipsumLorem ipsumмLorem ipsumLorem ipsum Lorem ipsum"
+        />
+
+        <Skill
+          iconId="figma"
+          title="web design"
+          description="Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum  Lorem ipsumLorem ipsumLorem ipsumLorem ipsum Lorem ipsumLorem ipsumLorem ipsum Lorem ipsumLorem ipsumмLorem ipsumLorem ipsum Lorem ipsum"
+        />
+      </FlexWrapper>
+    </StyledSkills>
+  );
+};
+
+const StyledSkills = styled.section`
+  background-color: #e6ffe9;
+  min-height: 100vh;
+`;
